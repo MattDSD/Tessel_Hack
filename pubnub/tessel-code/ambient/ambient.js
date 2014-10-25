2c2a6f23ae3524abfeb0e8eb2cdfaa4311a5d4ab
Kim Marquardt <kim.marquardt@gmail.com>
2014-10-25 13:12:33 -0700

    Complete basic tessel->ambient->pubnub connection

--- pubnub/tessel-code/ambient/ambient.js
+++ pubnub/tessel-code/ambient/ambient.js
@@ -1,34 +1,32 @@
- function publish() {
+ function publish(ambientData) {
 
   pubnub = PUBNUB.init({
      publish_key   : 'demo',
      subscribe_key : 'demo'
   });
 
-  // pubnub.subscribe({
-  //    channel : "surprise_party",
-  //    message : function(message,envelope,channel){
-  //      console.log('message', 'envelope', 'channel');
-  //    },
-  //    connect: pub
-  // });
+  // Tell SDK to subscribe and upon connection publish [ambientData]
+  pubnub.subscribe({
+     channel : "surprise_party",
+     message : function(message,envelope,channel){
+       console.log('message', 'envelope', 'channel');
+     },
+     connect: pub
+  });
 
+  // Tell SDK to pusblish to pubnub
   function pub() {
     pubnub.publish({
-      channel : "surprise_party",
-      message : "Hello SURPRISE!!!",
+      channel : "surprise_party,
+      message : ambientData",
       callback: function(publishCallbackResponse){ console.log(publishCallbackResponse) }
     });
   }
  };
 
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
-
 /*********************************************
-This ambient module example console.logs
-ambient light and sound levels and whenever a
-specified light or sound level trigger is met.
+This ambient module publishes {light 0.0f8 - 1.0f8, sound: 0.0f8-1.0f8} JSON
+every half second
 *********************************************/
 var pubnub = require("pubnub-hackathon").init({publish_key: "demo", subscribe_key: "demo" });
 var tessel = require('tessel');
@@ -43,7 +41,9 @@ ambient.on('ready', function () {
       if (err) throw err;
       ambient.getSoundLevel( function(err, sdata) {
         if (err) throw err;
-        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
+        // console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
+        var JSONPayload = JSON.stringify({light: ldata, sound: sdata});
+        publish(JSONPayload);
     });
   })}, 500); // The readings will happen every .5 seconds unless the trigger is hit
 
@@ -54,43 +54,43 @@ ambient.on('ready', function () {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ambient.setLightTrigger(0.5);
+//   ambient.setLightTrigger(0.5);
 
-  // Set a light level trigger
-  // The trigger is a float between 0 and 1
-  ambient.on('light-trigger', function(data) {
-    console.log("Our light trigger was hit:", data);
+//   // Set a light level trigger
+//   // The trigger is a float between 0 and 1
+//   ambient.on('light-trigger', function(data) {
+//     console.log("Our light trigger was hit:", data);
 
-    // Clear the trigger so it stops firing
-    ambient.clearLightTrigger();
-    //After 1.5 seconds reset light trigger
-    setTimeout(function () {
+//     // Clear the trigger so it stops firing
+//     ambient.clearLightTrigger();
+//     //After 1.5 seconds reset light trigger
+//     setTimeout(function () {
 
-        ambient.setLightTrigger(0.5);
+//         ambient.setLightTrigger(0.5);
 
-    },1500);
-  });
+//     },1500);
+//   });
 
-  // Set a sound level trigger
-  // The trigger is a float between 0 and 1
-  ambient.setSoundTrigger(0.1);
+//   // Set a sound level trigger
+//   // The trigger is a float between 0 and 1
+//   ambient.setSoundTrigger(0.1);
 
-  ambient.on('sound-trigger', function(data) {
-    console.log("Something happened with sound: ", data);
+//   ambient.on('sound-trigger', function(data) {
+//     console.log("Something happened with sound: ", data);
 
-    // Clear it
-    ambient.clearSoundTrigger();
+//     // Clear it
+//     ambient.clearSoundTrigger();
 
-    //After 1.5 seconds reset sound trigger
-    setTimeout(function () {
+//     //After 1.5 seconds reset sound trigger
+//     setTimeout(function () {
 
-        ambient.setSoundTrigger(0.1);
+//         ambient.setSoundTrigger(0.1);
 
-    },1500);
+//     },1500);
 
-  });
-});
+//   });
+// });
 
 ambient.on('error', function (err) {
-  console.log(err)
+  console.log(err);
 });
